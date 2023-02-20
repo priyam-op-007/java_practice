7ec29c6c83488eab01b6e0a9a0b6ec9447b8743d
Priyaranjan Mishra <priyaranjanmishra000@gmail.com>
2023-02-20 10:54:50 +0530

    completed palindrome_check.java

--- src/palindrome_check.java
+++ src/palindrome_check.java
@@ -1,2 +1,27 @@
+import java.util.*;
+
 public class palindrome_check {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String a = sc.nextLine();
+        boolean palindrome = true;
+        int start = 0;
+        int end = a.length() - 1;
+        while (start < end ){
+            if (a.charAt(start) != a.charAt(end)){
+                palindrome = false;
+                break;
+
+            }
+            start++;
+            end--;
+        }
+        if (palindrome){
+            System.out.println("Its a palindromic string");
+        }
+        else{
+            System.out.println("Its not a palindromic string");
+        }
+
+    }
 }
